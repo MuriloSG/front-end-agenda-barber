4bffa77830f3a6b5e87fb60f7c31453638e88fff
MuriloSG <muriloprojetosti@gmail.com>
2025-03-18 00:43:54 -0300

    :wrench: chore: adicionar componentes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["lfulpyqhzcddnlfxedja.supabase.co"], 
+  },
+};
 
 export default nextConfig;
