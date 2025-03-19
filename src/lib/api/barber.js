6cf8b636f167acc15df4460ecc3a4df0807d3d97
MuriloSG <muriloprojetosti@gmail.com>
2025-03-18 22:54:01 -0300

    :sparkles: feat: adicionar funcoes de buscar servico pelo id e funcao de atualizar

--- src/lib/api/barber.js
+++ src/lib/api/barber.js
@@ -41,6 +41,29 @@ export async function getBarberServices() {
   return result;
 }
 
+export async function getServiceById(serviceId) {
+  const TOKEN = Cookies.get("token");
+  if (!TOKEN) {
+    throw new Error("Token não encontrado nos cookies");
+  }
+
+  const response = await fetch(`${API_URL}/services/${serviceId}/`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Token ${TOKEN}`,
+    },
+  });
+
+  const result = await response.json();
+
+  if (!response.ok) {
+    throw new Error(result.non_field_errors || "Erro ao buscar o serviço");
+  }
+
+  return result;
+}
+
 export async function createService(data) {
   console.log(data)
   const TOKEN = Cookies.get("token");
@@ -50,6 +73,31 @@ export async function createService(data) {
 
   const response = await fetch(`${API_URL}/services/`, {
     method: "POST",
+    headers: {
+      Authorization: `Token ${TOKEN}`,
+    },
+    body: data,
+  });
+  const result = await response.json();
+  if (!response.ok) {
+    throw new Error(
+      result.non_field_errors
+        ? result.non_field_errors.join(", ")
+        : "Erro desconhecido"
+    );
+  }
+  return result;
+}
+
+export async function updateService(serviceId, data) {
+  console.log(data);
+  const TOKEN = Cookies.get("token");
+  if (!TOKEN) {
+    throw new Error("Token não encontrado nos cookies");
+  }
+
+  const response = await fetch(`${API_URL}/services/${serviceId}/`, {
+    method: "PUT",
     headers: {
       Authorization: `Token ${TOKEN}`,
     },
