52d77c799f083d398368c18e8dad890f37371e20
MuriloSG <muriloprojetosti@gmail.com>
2025-03-23 17:28:14 -0300

    feat: ajuste middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,24 +1,45 @@
 import { NextResponse } from "next/server";
 
 export function middleware(req) {
-    const token = req.cookies.get("token");
-    const userCookie = req.cookies.get("user");
+  const token = req.cookies.get("token");
+  const userCookie = req.cookies.get("user");
 
-    if (!token || !userCookie) {
-        return NextResponse.redirect(new URL("/login", req.url));
-    }
-    const user = JSON.parse(userCookie.value);
-    if (user.profile_type === "barbeiro" && req.url.includes("/dashboard/customers")) {
-        return NextResponse.redirect(new URL("/dashboard/barbers", req.url));
-    }
-
-    if (user.profile_type === "cliente" && req.url.includes("/dashboard/barbers")) {
-        return NextResponse.redirect(new URL("/dashboard/customers", req.url));
-    }
-
+  if (!token || !userCookie) {
     return NextResponse.next();
+  }
+
+  const user = JSON.parse(userCookie.value);
+
+  if (req.url.includes("/login") || req.url.includes("/register")) {
+    const dashboardRoute =
+      user.profile_type === "barbeiro"
+        ? "/dashboard/barbers"
+        : "/dashboard/customers";
+    return NextResponse.redirect(new URL(dashboardRoute, req.url));
+  }
+
+  if (
+    user.profile_type === "barbeiro" &&
+    req.url.includes("/dashboard/customers")
+  ) {
+    return NextResponse.redirect(new URL("/dashboard/barbers", req.url));
+  }
+
+  if (
+    user.profile_type === "cliente" &&
+    req.url.includes("/dashboard/barbers")
+  ) {
+    return NextResponse.redirect(new URL("/dashboard/customers", req.url));
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
-    matcher: ["/dashboard/barbers/:path*", "/dashboard/customers/:path*"],
+  matcher: [
+    "/dashboard/barbers/:path*",
+    "/dashboard/customers/:path*",
+    "/login",
+    "/register",
+  ],
 };
